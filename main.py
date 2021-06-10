d786378cdec535ad3941ba321ab3729973aa85a3
Kento Nishi <kento24gs@outlook.com>
2021-06-09 21:28:32 -0700

    write to readme

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import arxiv
 import latexcodec
+from tomark import Tomark
 
 # %%
 
@@ -14,11 +15,26 @@ def d(s):
 
 search = arxiv.Search(
     query='ti:"is all you need"',
-    max_results=float('inf'),
+    max_results=float("inf"),
     sort_by=arxiv.SortCriterion.SubmittedDate,
+    sort_order=arxiv.SortOrder.Ascending,
 )
 
+table = []
+
 for result in search.get():
-    print(d(result.title), result)
+    etal = " et al." if len(result.authors) > 1 else ""
+    table.append(
+        {
+            "Title": f"[{d(result.title)}]({result.entry_id})",
+            "Authors": f"{result.authors[0].name}{etal}",
+            "Date": str(result.published).split(" ")[0],
+        }
+    )
 
 # %%
+
+with open("template.md", "r", encoding="utf8") as file:
+    data = file.read().replace("[PAPER LIST]", Tomark.table(table))
+with open("readme.md", "w", encoding="utf8") as file:
+    file.writelines(data)
---
+++ template.md
@@ -0,0 +1,20 @@
+# Awesome <u>***"is all you need"***</u> papers
+
+## About
+
+This repository is a list of all "is all you need" papers. The list is updated daily using the arXiv API.
+
+## Paper List
+
+[PAPER LIST]
+
+## FAQ
+
+### Why is it automated?
+Because I'm lazy.
+
+### Why is it only updated daily?
+The arXiv API only updates search queries once a day.
+
+### How can I contribute?
+Please feel free to submit a PR!
